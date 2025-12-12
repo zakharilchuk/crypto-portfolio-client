82fd03b17b82cca46f320c29c9a5265569c2bb0f
Zakhar Ilchuk <zakharilchuk1@gmail.com>
2025-12-12 22:48:53 +0200

    add axios instance for protected routes

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+function DashboardPage() {
+  
+  useEffect(() => {
+
+  })
+
+  return <div>Welcome to the Dashboard!</div>;
+}
+
+export default DashboardPage;
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,51 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: "http://localhost:8080/api/",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   withCredentials: true,
 });
+
+export const authInstance = axios.create({
+  baseURL: "http://localhost:8080/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+authInstance.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
+  return config;
+});
+
+authInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const res = await authInstance.post("/auth/refresh", {
+          withCredentials: true,
+        });
+        localStorage.setItem("accessToken", res.data.accessToken);
+        return authInstance.request(originalRequest);
+      } catch (err) {
+        localStorage.removeItem("accessToken");
+        console.log(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
